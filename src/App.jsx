ce113dedb9ee30e0e1964d1cde480150c31ded5b
nazar rachun <nazarrachun@gmail.com>
2017-08-14 12:15:39 +0300

    commit in to new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      kilometers: 0
+    };
+
+    this.changeKilometers = this.changeKilometers.bind(this);
+  }
+  changeKilometers() {
+    this.setState({ kilometers: this.kilometers.value });
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <input type="text" value={this.state.kilometers} ref={(node) => { this.kilometers = node; }} onChange={this.changeKilometers} /><br />
+        <label>{7 * this.state.kilometers}</label>
+      </div>
     );
   }
 }
