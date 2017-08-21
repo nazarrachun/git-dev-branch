2f2a0bf5174bb3fd96d649fe1f250c6e437f34a1
nazar rachun <nazarrachun@gmail.com>
2017-08-21 10:40:13 +0300

    next com

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,27 @@ class App extends Component {
     super();
 
     this.state = {
-      kilometers: 0
+      kilometers: 0,
+      speed: 0
     };
 
     this.changeKilometers = this.changeKilometers.bind(this);
+    this.changeSpeed = this.changeSpeed.bind(this);
   }
   changeKilometers() {
     this.setState({ kilometers: this.kilometers.value });
+  }
+  changeSpeed() {
+    this.setState({ speed: this.speed.value });
   }
 
   render() {
     return (
       <div>
-        <input type="text" value={this.state.kilometers} ref={(node) => { this.kilometers = node; }} onChange={this.changeKilometers} /><br />
-        <label>{7 * this.state.kilometers}</label>
+        <p><input type="text" value={this.state.kilometers} ref={(node) => { this.kilometers = node; }} onChange={this.changeKilometers} /><br />
+        <label>{7 * this.state.kilometers}</label></p>
+        <p><input type="text" value={this.state.speed} ref={(nodes) => { this.speed = nodes; }} onChange={this.changeSpeed} /><br />
+        <label>{this.state.speed * this.state.kilometers + 7} </label></p>
       </div>
     );
   }
